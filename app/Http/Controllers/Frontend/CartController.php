bb7413739194f9c670c013f22b2316290b374c23
dongthuythanhh <thanhthanhdong2003@gmail.com>
2024-04-10 00:24:04 +0700

    refs: fix layout do merge

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -20,20 +20,8 @@ class CartController extends Controller
     {
 
         $loai_phong = Loai_phong::findOrFail($request->id);
-        // $so_luong = $request->so_luong;
-        $so_luong = Phong::where('loai_phong_id', $loai_phong->id)->count();
-
-        // Kiểm tra số lượng phòng cần đặt có vượt quá số phòng còn trống không
-        // $so_phong_trong = Phong::where('loai_phong_id', $loai_phong->id)->count();
-        // if ($so_luong > $so_phong_trong) {
-        //     return response(['status' => 'Lỗi', 'message' => 'Số lượng phòng đặt vượt quá số phòng còn trống']);
-        // }
-
-        $request->session()->put('ngayBatDau', $request->ngayBatDau);
-        $request->session()->put('ngayKetThuc', $request->ngayKetThuc);
-
-        $so_luong = Phong::where('loai_phong_id', $loai_phong->id)->whereDoesntHave('datPhongs', function ($query) {
-            $query->where('thoi_gian_den', '<', Carbon::now())->where('thoi_gian_di', '>', Carbon::now());
+        $weight = Phong::where('loai_phong_id', $loai_phong->id)->whereDoesntHave('datPhongs', function ($query) {
+            // $query->where('thoi_gian_den', '<', Carbon::now())->where('thoi_gian_di', '>', Carbon::now());
         })->count();
     
         $cartData = [];
@@ -41,26 +29,18 @@ class CartController extends Controller
         $cartData['name'] = $loai_phong->ten;
         $cartData['price'] = $loai_phong->gia;
         $cartData['qty'] = 1;
-        $cartData['weight'] = 10;
-
+        $cartData['weight'] = $weight;
         $cartData['ngay_bat_dau'] = $request->ngayBatDau;
         $cartData['ngay_ket_thuc'] = $request->ngayKetThuc;
-        // $cartData['image'] = $loai_phong->anh;
-        $cartData['options']['image'] = $loai_phong->anh;
-        // $cartData['gia_ban_dau'] = $loai_phong->gia_ban_dau;
-        // $cartData['gioi_han_nguoi'] = $loai_phong->gioi_han_nguoi;
-        Cart::add($cartData);
-        return response(['status' => 'Thành công', 'message' => 'Thêm giỏ hàng thành công']);
         $cartData['options']['image'] = $loai_phong->anh;
     
         // Kiểm tra nếu số lượng phòng trống đủ để thêm vào giỏ hàng
-        if ($cartData['qty'] <= $so_luong) {
+        if ($cartData['qty'] <= $weight) {
             Cart::add($cartData);
             return Redirect::route('kiem_tra_phong')->with(['status' => 'success', 'message' => 'Thêm vào giỏ hàng thành công']);
         } else {
             return Redirect::route('kiem_tra_phong')->with(['status' => 'error', 'message' => 'Không đủ phòng trống để thêm vào giỏ hàng']);
         }
-
     }
 
     public function cartDetail(Request $request)
--- resources/views/client/pages/cart-detail.blade.php
+++ resources/views/client/pages/cart-detail.blade.php
@@ -104,241 +104,179 @@
                         </tbody>
                     </table>
                 </div>
+            </div>
+        </div>
 
+        {{-- div 2 --}}
+
+        <div class="bg-white rounded-lg overflow-hidden shadow-md">
+            <div class="p-4 border-b">
+                <h5 class="text-lg font-semibold">THÔNG TIN ĐẶT PHÒNG</h5>
             </div>
             <div class="p-4">
-                <table class="w-full table-fixed">
-                    <thead>
-                        <tr>
-                            <th class="px-2 py-2">#</th>
-                            <th class="px-2 py-2">Loại phòng</th>
-                            <th class="px-2 py-2">Số lượng</th>
-                            <th class="px-2 py-2">Giá</th>
-                            <th class="px-2 py-2">Tổng tiền</th>
-                            <th class="px-2 py-2">
-                                <button class="btn btn-primary ">Clear</button>
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($cartItems as $item)
-                        <tr>
-                            <td>
-                                <img src="{{ Storage::url($item->image) }}" alt="" class="w-24 h-auto">
-                            </td>
-                            <td>
-                                <p>{{ $item->name }}</p>
 
-                                <input type="text" name="ngayBatDau" value="{{$ngayBatDau}}">
-                                <input type="text" name="ngayKetThuc" value="{{$ngayKetThuc}}">
+                <form class="mb-3" action="{{ route('checkout') }}" method="get">
+                    @csrf
 
-                            </td>
+                    <div class="flex items-center justify-between mb-2 mt-2">
+                        <p class="font-semibold">Tổng tiền:</p>
+                        <p id="total" class="font-semibold">{{ number_format($total * $soNgay, 0, '.', '.') }}VNĐ</p>
+                        <input type="hidden" value="{{ $total }}" name="total">
+                    </div>
 
-                            <td>
-                                <div class="flex items-center">
-                                    <button class="btn btn-warning mr-1 room-decrement">
-                                        <i class="bi bi-dash"></i>
-                                    </button>
-                                    <input type="text" class="form-control w-16 px-2 py-1 text-center phong-qty" value="{{ $item->qty }}" readonly data-rowid="{{ $item->rowId }}" max="">
-                                    <button class="btn btn-success ml-1 room-increment">
-                                        <i class="bi bi-plus-lg"></i>
-                                    </button>
-                                </div>
-                            </td>
+                    <div class="flex items-center justify-between mb-2 mt-2">
+                        <p class="font-semibold">Khuyến mãi:</p>
+                        <p id="discount" class="font-semibold">0VNĐ</p>
+                    </div>
 
-                            <td>{{ number_format($item->price, 0, '.', '.')  }}VNĐ</td>
-                            <td id="{{ $item->rowId }}">
-                                {{ number_format($item->price * $item->qty, 0, '.', '.') }}VNĐ
-                            </td>
-                            <td>
-                                <a href="{{ route('chi_tiet_gio_hang.xoa_loai_phong', $item->rowId) }}" class="btn btn-danger">
-                                    <i class="bi bi-trash"></i>
-                                </a>
-                            </td>
-                        </tr>
-                        @endforeach
-                        @if (count($cartItems) == 0)
-                        <tr>
-                            <td class="border-b-0">Bạn chưa chọn phòng nào !</td>
-                        </tr>
-                        @endif
-                    </tbody>
-                </table>
+                    <div class="flex items-center justify-between mt-2">
+                        <p class="font-semibold">Thành tiền:</p>
+                        <p id="cart_total" class="font-semibold">{{ number_format($total * $soNgay, 0, '.', '.') }}VNĐ</p>
+
+                        <input type="hidden" value="{{ $total }}" name="cart_total" id="input_cart_total">
+                    </div>
+
+                    <div class="mt-3">
+                        <span class="font-semibold">Áp dụng mã giảm giá</span>
+
+                        {{-- <form id="coupon_form" class="flex mb-3 mt-2"> --}}
+
+                        <input class="form-control w-40 px-2 py-1 mr-2 border-2" type="text" placeholder="Mã khuyến mãi" name="coupon_code">
+                        <button class="py-1 px-2 inline-block font-semibold tracking-wide align-middle duration-500 text-base text-center bg-red-500 text-white rounded-md">Áp
+                            dụng</button>
+                        {{-- </form> --}}
+                    </div>
+
+                    @if (count($cartItems) != 0)
+                    <button class="mt-4 py-1 px-5 h-10 inline-block tracking-wide align-middle duration-500 text-base text-center bg-red-500 text-white rounded-md w-full cursor-pointer" type="submit">Đặt phòng</button>
+                    @endif
+
+                </form>
+
+
+
             </div>
         </div>
+
     </div>
-
-    {{-- div 2 --}}
-
-    <div class="bg-white rounded-lg overflow-hidden shadow-md">
-        <div class="p-4 border-b">
-            <h5 class="text-lg font-semibold">THÔNG TIN ĐẶT PHÒNG</h5>
-        </div>
-        <div class="p-4">
-
-            <form class="mb-3" action="{{ route('checkout') }}" method="get">
-                @csrf
-
-                <input type="text" name="ngayBatDau" value="{{$ngayBatDau}}">
-                <input type="text" name="ngayKetThuc" value="{{$ngayKetThuc}}">
-
-                <div class="flex items-center justify-between mb-2 mt-2">
-                    <p class="font-semibold">Tổng tiền:</p>
-
-                    <p id="total" class="font-semibold">{{ number_format($total * $soNgay, 0, '.', '.') }}VNĐ</p>
-                    <input type="hidden" value="{{ $total }}" name="total">
-                </div>
-
-                <div class="flex items-center justify-between mb-2 mt-2">
-                    <p class="font-semibold">Khuyến mãi:</p>
-                    <p id="discount" class="font-semibold">0VNĐ</p>
-                </div>
-
-                <div class="flex items-center justify-between mt-2">
-                    <p class="font-semibold">Thành tiền:</p>
-                    <p id="cart_total" class="font-semibold">{{ number_format($total * $soNgay, 0, '.', '.') }}VNĐ</p>
-                    <input type="hidden" value="{{ $total * $soNgay }}" name="cart_total" id="input_cart_total">
-                </div>
-
-                <div class="mt-3">
-                    <span class="font-semibold">Áp dụng mã giảm giá</span>
-
-                    {{-- <form id="coupon_form" class="flex mb-3 mt-2"> --}}
-
-                    <input class="form-control w-40 px-2 py-1 mr-2 border-2 mt-2" type="text" placeholder="Mã khuyến mãi" name="coupon_code">
-                    <button class="py-1 px-2 inline-block font-semibold tracking-wide align-middle duration-500 text-base text-center bg-red-500 text-white rounded-md">Áp
-                        dụng</button>
-                    {{-- </form> --}}
-                </div>
-
-                @if (count($cartItems) != 0)
-                <button class="mt-4 py-1 px-5 h-10 inline-block tracking-wide align-middle duration-500 text-base text-center bg-red-500 text-white rounded-md w-full cursor-pointer" type="submit">Đặt phòng</button>
-                @endif
-
-            </form>
-
-
-
-        </div>
-    </div>
-
-</div>
 </div>
 @endsection
 
 @push('scripts')
 <script>
     $(document).ready(function() {
-                $('.room-increment').on('click', function() {
-                    alert(123);
-                    let input = $(this).siblings('.phong-qty');
-                    let quantity = parseInt(input.val()) + 1;
-                    console.log(quantity);
-                    let rowId = input.data('rowid');
-                    $.ajax({
-                        url: "{{ route('chi_tiet_gio_hang.them_phong') }}",
-                        method: "POST",
-                        data: {
-                            quantity: quantity,
-                            rowId: rowId
-                        },
-                        success: function(data) {
-                            console.log(data);
-                            if (data.status === 'success') {
-                                input.val(quantity);
-                                let id = "#" + rowId;
-                                toastr.success(data.message);
-                                $(id).text(data.phong_total + "VNĐ");
-                                $('#total').text(data.total + "VNĐ");
-                                calcCouponDiscount();
-                            } else if (data.status === 'error') {
-                                toastr.error(data.message);
-                            }
-                        }
-                    });
-                });
-
-                $('.room-decrement').on('click', function() {
-                    let input = $(this).siblings('.phong-qty');
-                    let quantity = parseInt(input.val()) - 1;
-                    if (quantity < 1) {
-                        quantity = 1;
+        $('.room-increment').on('click', function() {
+            alert(123);
+            let input = $(this).siblings('.phong-qty')
+            let quantity = parseInt(input.val()) + 1;
+            console.log(quantity);
+            let rowId = input.data('rowid')
+            $.ajax({
+                url: "{{ route('chi_tiet_gio_hang.them_phong') }}",
+                method: "POST",
+                data: {
+                    quantity: quantity,
+                    rowId: rowId
+                },
+                success: function(data) {
+                    console.log(data);
+                    if (data.status === 'success') {
+                        input.val(quantity)
+                        let id = "#" + rowId;
+                        toastr.success(data.message)
+                        $(id).text(data.phong_total + "VNĐ")
+                        VNĐ('#total').text(data.total + "VNĐ");
+                        calcCouponDiscount()
+                    } else if (data.status === 'error') {
+                        toastr.error(data.message)
                     }
-                    input.val(quantity);
-                    let rowId = input.data('rowid');
-                    $.ajax({
-                        url: "{{ route('chi_tiet_gio_hang.them_phong') }}",
-                        method: "POST",
-                        data: {
-                            quantity: quantity,
-                            rowId: rowId
-                        },
-                        success: function(data) {
-                            console.log(data);
-                            if (data.status === 'success') {
-                                let id = "#" + rowId;
-                                toastr.success(data.message);
-                                $(id).text(data.phong_total + "VNĐ");
-                                $('#total').text(data.total + "VNĐ");
-                                calcCouponDiscount();
-                            }
-                        }
-                    });
-                });
+                }
+            })
+        })
 
-                $('.clear-cart').on('click', function() {
-                    Swal.fire({
-                        title: 'Bạn có muốn xóa?',
-                        text: "Hành động này sẽ xóa loại phòng!",
-                        icon: 'warning',
-                        showCancelButton: true,
-                        confirmButtonColor: '#3085d6',
-                        cancelButtonColor: '#d33',
-                        confirmButtonText: 'Đồng ý'
-                    }).then((result) => {
-                        if (result.isConfirmed) {
-                            $.ajax({
-                                type: 'GET',
-                                url: "{{ route('clear-cart') }}",
-                                success: function(data) {
-                                    console.log(data);
-                                    if (data.status == 'success') {
-                                        Swal.fire(
-                                            'Xóa!',
-                                            data.message,
-                                            'success'
-                                        );
-                                        window.location.reload();
-                                    }
-                                },
-                                error: function(xhr, status, error) {
-                                    console.log(error);
-                                }
-                            });
-                        }
-                    });
-                });
+        $('.room-decrement').on('click', function() {
+            let input = $(this).siblings('.phong-qty')
+            let quantity = parseInt(input.val()) - 1;
+            if (quantity < 1) {
+                quantity = 1
+            }
+            input.val(quantity)
+            // console.log(quantity);
+            let rowId = input.data('rowid')
+            $.ajax({
+                url: "{{ route('chi_tiet_gio_hang.them_phong') }}",
+                method: "POST",
+                data: {
+                    quantity: quantity,
+                    rowId: rowId
+                },
+                success: function(data) {
+                    console.log(data);
+                    if (data.status === 'success') {
+                        let id = "#" + rowId;
+                        toastr.success(data.message)
+                        $(id).text(data.phong_total + "VNĐ")
+                        $('#total').text(data.total + "VNĐ");
+                        calcCouponDiscount()
+                    }
+                }
+            })
+        })
 
-                function calcCouponDiscount() {
+        $('.clear-cart').on('click', function() {
+            Swal.fire({
+                title: 'Bạn có muốn xóa?',
+                text: "Hành động này sẽ xóa loại phòng!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Đồng ý'
+            }).then((result) => {
+                if (result.isConfirmed) {
+
                     $.ajax({
                         type: 'GET',
-                        url: "{{ route('coupon-calc') }}",
+                        url: "{{ route('clear-cart') }}",
                         success: function(data) {
                             console.log(data);
-                            if (data.status == 'error') {
-                                toastr.error(data.message);
-                            } else if (data.status == 'success') {
-                                $('#discount').text(data.discount + 'VNĐ');
-                                $('#cart_total').text(data.cart_total + 'VNĐ');
-                                $('#input_cart_total').val(data.cart_total);
+                            if (data.status == 'success') {
+                                Swal.fire(
+                                    'Xóa!',
+                                    data.message,
+                                    'success'
+                                )
+                                window.location.reload();
                             }
                         },
                         error: function(xhr, status, error) {
-                            toastr.error('Lỗi khi gọi AJAX.');
-                            console.error(xhr.responseText);
+                            console.log(error);
                         }
-                    });
+                    })
                 }
-            });
+            })
+        })
+
+        function calcCouponDiscount() {
+            $.ajax({
+                type: 'GET',
+                url: "{{ route('coupon-calc') }}",
+                success: function(data) {
+                    console.log(data);
+                    if (data.status == 'error') {
+                        toastr.error(data.message)
+                    } else if (data.status == 'success') {
+                        $('#discount').text(data.discount + 'VNĐ');
+                        $('#cart_total').text(data.cart_total + 'VNĐ');
+                        $('#input_cart_total').val(data.cart_total);
+                    }
+                }
+            })
+        }
+    })
 </script>
+
+
+
+
 @endpush
